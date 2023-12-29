094cf9693cf3a494a25ae9fd41135cd70efe5d19
hywax <a.hywax@gmail.com>
2023-12-29 18:04:31 +0500

    chore: vite configuration

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { resolve } from 'node:path'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+import camelCase from 'camelcase'
+import packageJson from './package.json'
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs', 'umd', 'iife'],
+      name: camelCase(packageJson.name, { pascalCase: true }),
+      fileName: packageJson.name,
+    },
+  },
+  plugins: [
+    dts({ rollupTypes: true }),
+  ],
+})
