6755656d9f41473ade96b4a197feaa8dcaa02f04
hywax <a.hywax@gmail.com>
2023-12-29 16:47:31 +0500

    feat(style): eslint configuration

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: true,
+  ignores: [
+    'dist',
+    'node_modules',
+  ],
+  rules: {
+    'style/brace-style': ['error', '1tbs'],
+    'style/arrow-parens': ['error', 'always'],
+    'curly': ['error', 'all'],
+    'antfu/consistent-list-newline': 'off',
+  },
+})
